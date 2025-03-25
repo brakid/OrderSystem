d70436b1ce9bbdb3003e331d77bcd41a1656b5da
Hagen Schupp <hagen.schupp@rwth-aachen.de>
2025-03-25 23:40:55 +0100

    Testing Mutations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import type { Book, Resolvers } from './gql/resolvers-types';
 import { readFileSync } from 'fs';
+import { randomUUID } from 'crypto';
 
 const typeDefs = gql(
   readFileSync('schema.graphql', {
@@ -12,10 +13,12 @@ const typeDefs = gql(
 
 const books: Book[] = [
   {
+    id: randomUUID().toString(),
     title: 'The Awakening',
     author: 'Kate Chopin',
   },
   {
+    id: randomUUID().toString(),
     title: 'City of Glass',
     author: 'Paul Auster',
   },
@@ -23,9 +26,32 @@ const books: Book[] = [
 
 const resolvers: Resolvers = {
   Query: {
-    books: (): Book[] => books,
-    book: (parent, args, contextValue, info): Book => books.find(book => book.title === args.title) as Book
+    getBooks: (): Book[] => books,
+    getBook: (parent, args, contextValue, info): Book => books.find(book => book.title === args.title) as Book
   },
+  Mutation: {
+    updateBook: (parent, args, contextValue, info): Book => {
+      const book = books.find(book => book.id == args.id);
+      if (!!book) {
+        book.title = args.title;
+        book.author = args.author;
+        return book;
+      } else {
+        throw new Error(`No book with ID ${args.id} found`);
+      }
+    },
+    createBook: (parent, args, contextValue, info): Book => {
+      const book = {
+        id: randomUUID().toString(),
+        title: args.title,
+        author: args.author,
+      }
+
+      books.push(book);
+
+      return book;
+    }
+  }
 };
 
 const server = new ApolloServer({
